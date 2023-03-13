9c2e27c3ecd166755cfcad6b2a8a5990a47168ad
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2023-03-13 18:38:42 +0300

    Create Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="Header">
+      <a href="#">News</a>
+      <a href="#">Messages</a>
+      <a href="#">Videos</a>
+    </nav>
+  )
+}
+export default Header;
