97bdec0b43223fd221f185c0a36e46db27aeffd4
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2023-03-18 21:18:35 +0300

    Add newPost function

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,12 +4,19 @@ import s from './Profile.module.css';
 
 const Profile = (props) => {
   let posts = props.data.posts.map(post => <Post text={post.text} />)
-
+  let postText = React.createRef();
+  let newPost = () => {
+    let postMessage = postText.current.value;
+    alert(postMessage)
+  }
   return (
+
     <div className={s.profile}>
       <div>{props.data.avatar}</div>
       <div>{props.data.name}</div>
       <div> {props.data.description}</div>
+      <textarea ref={postText} cols="30" rows="2"></textarea>
+      <button onClick={newPost}>New post</button>
       <div>{posts}</div>
     </div>
   )
